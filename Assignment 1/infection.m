8e4024772334d8d2780f8af90b2bb58b09ddf54b
elliotrussyu <elliotrussyu@icloud.com>
2021-02-16 20:29:16 +0100

    Some modifications

--- Assignment 1/infection.m
+++ Assignment 1/infection.m
@@ -8,20 +8,20 @@ T = max(data(:,3));
 rec = zeros(N,T+1);
 for i = 1:N
 
-    bad_node = [i];
-    infected = length(bad_node);
+    bad_node = [i]; % The log of infected nodes in each iteration
+    infected = length(bad_node); %Calculate how many nodes are infected
     rec(i,1) = infected;
     for t = 1:T        
-        index = find(data(:,3) == t);
-        st = data(index,1);
-        st1 = unique(st);
-        en = data(index,2);
+        index = find(data(:,3) == t); %Extract the segment of the data corresponds to the current time stamp
+        st = [data(index,1);data(index,2)];% The starting nodes, since we have an undirected graph, we would establish the matrix as [L+;L-] 
+        st1 = unique(st); %The starting nodes at the time stamp t
+        en = [data(index,2);data(index,1)]; %The ending nodes at time stamp t(both directions)
         ind = [];
         for s = 1:length(st1)
-            ind0 = find(bad_node == st1(s));
-            if isempty(ind0) == 0
+            ind0 = find(bad_node == st1(s)); %Find if the starting nodes contains any of the infected nodes
+            if isempty(ind0) == 0 %Check there is any infected nodes sending out messages
                 for in = 1:length(ind0)
-                    ind = [ind (find(st == bad_node(ind0(in))))'];
+                    ind = [ind (find(st == bad_node(ind0(in))))']; %Get the corresponding location
                 end
             end     
         end
